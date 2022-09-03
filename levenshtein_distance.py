57b8a6c3174d23d4dd6aeb9c8384eaf1e73ef31f
Syn-dromatic <68112904+Syn-dromatic@users.noreply.github.com>
2022-09-03 13:38:59 +0300

    update

--- levenshtein_distance.py
+++ levenshtein_distance.py
@@ -32,7 +32,7 @@ def create_sequence_data(seq1: str, seq2: str) -> tuple[dict, np.ndarray]:
     return sequence_dict, zero_seq_arr
 
 
-def insert_null_onset(seq1, seq2):
+def insert_null_onset(seq1: list, seq2: list) -> tuple[list, list]:
     seq1.insert(0, " ")
     seq2.insert(0, " ")
     return seq1, seq2
@@ -69,14 +69,9 @@ def delete_operation(x: int, y: int) -> tuple[int, int]:
     return x, y
 
 
-def filter_ops_dict(ops_dict: dict) -> dict:
-    keys_pop = []
-    for key, values in ops_dict.items():
-        if values["val"] is None:
-            keys_pop.append(key)
-    for key in keys_pop:
-        ops_dict.pop(key)
-    return ops_dict
+def filter_ops_dict(ops_list: list) -> list:
+    filtered_ops = [op for op in ops_list if op["val"] is not None]
+    return filtered_ops
 
 
 def dynamic_operations(x: int, y: int, seq_dict: dict, seq_arr: np.ndarray) -> dict:
@@ -91,25 +86,25 @@ def dynamic_operations(x: int, y: int, seq_dict: dict, seq_arr: np.ndarray) -> d
     rep_val = seq_arr[y_rep][x_rep] if (x_rep >= 0 and y_rep >= 0) else None
     del_val = seq_arr[y_del][x_del] if (x_del >= 0 and y_del >= 0) else None
 
-    ops_dict = {
-        "insert": {"x": x_ins, "y": y_ins, "val": ins_val, "key": "insert"},
-        "replace": {"x": x_rep, "y": y_rep, "val": rep_val, "key": "replace"},
-        "delete": {"x": x_del, "y": y_del, "val": del_val, "key": "delete"},
-    }
+    ops_list = [
+        {"x": x_ins, "y": y_ins, "val": ins_val, "key": "insert"},
+        {"x": x_rep, "y": y_rep, "val": rep_val, "key": "replace"},
+        {"x": x_del, "y": y_del, "val": del_val, "key": "delete"},
+    ]
 
-    ops_dict = filter_ops_dict(ops_dict)
+    ops_list = filter_ops_dict(ops_list)
 
-    if ops_dict:
+    if ops_list:
         value_key = "val"
-        lowest_value = min((int(d[value_key])) for d in ops_dict.values())
-        lowest_key = [
-            k for k in ops_dict if (int(ops_dict[k][value_key])) == lowest_value
-        ]
+        min_value = min((int(d[value_key])) for d in ops_list)
+        min_ops = [k for k in ops_list if (int(k[value_key])) == min_value]
+
+        min_op_dict = min_ops[0]
 
         if x_dict == y_dict:
-            x_m = ops_dict[lowest_key[0]]["x"]
-            y_m = ops_dict[lowest_key[0]]["y"]
-            op_values = {
+            x_m = min_op_dict["x"]
+            y_m = min_op_dict["y"]
+            op_dict = {
                 "x": x_m,
                 "y": y_m,
                 "val": seq_arr[y_m][x_m],
@@ -117,15 +112,15 @@ def dynamic_operations(x: int, y: int, seq_dict: dict, seq_arr: np.ndarray) -> d
             }
 
         else:
-            op_values = ops_dict[lowest_key[0]]
+            op_dict = min_op_dict
 
     else:
-        op_values = {"x": 0, "y": 0, "val": 0, "key": "beginning"}
+        op_dict = {"x": 0, "y": 0, "val": 0, "key": "beginning"}
 
-    return op_values
+    return op_dict
 
 
-def ops_incr_dict():
+def ops_incr_dict() -> dict[str, int]:
     ops_incr = {
         "beginning": 0,
         "matching": 0,
